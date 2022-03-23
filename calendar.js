6139c7b81148fa682964b80b1a838165e9acda38
dajin jung <dajin@blueprint-lab.com>
2022-03-23 21:01:30 +0900

    calendar

---
+++ calendar.js
@@ -0,0 +1,88 @@
+const date = new Date();
+
+const renderCalendar = () => {
+  
+  // var startDate = new Date(getFormElemVal(FRM_FILTER, 'text', 0),getFormElemVal(FRM_FILTER, 'text', 1) - 1,getFormElemVal(FRM_FILTER, 'text', 2));
+  // var endDate = new Date(getFormElemVal(FRM_FILTER, 'text', 3),getFormElemVal(FRM_FILTER, 'text', 4) - 1,getFormElemVal(FRM_FILTER, 'text', 5));
+  date.setDate(1);
+  // startDate.setDate(1);
+  // endDate.setDate(1);
+
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+//   document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
